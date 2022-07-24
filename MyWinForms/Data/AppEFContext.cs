7d192a33fbba476c79d3f709d644f7176919e8ef
novakvova <novakvova@gmail.com>
2022-07-24 17:07:43 +0300

    Add table migration tblUsers

--- MyWinForms/Data/AppEFContext.cs
+++ MyWinForms/Data/AppEFContext.cs
@@ -11,7 +11,7 @@ namespace MyWinForms.Data
     {
         public AppEFContext()
         {
-            this.Database.EnsureCreated();
+            //this.Database.EnsureCreated();
         }
 
         public DbSet<AppUser> Users { get; set; }
---
+++ MyWinForms/Migrations/20220724135834_Add table tblUsers.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWinForms.Migrations
+{
+    public partial class AddtabletblUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Photo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblUsers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblUsers");
+        }
+    }
+}
