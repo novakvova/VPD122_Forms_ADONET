a92a318deb6eb3f455baadb28be64b9a60bf1b3c
novakvova <novakvova@gmail.com>
2022-08-21 16:12:50 +0300

    Add category

--- MyWinForms/CategoryForm.Designer.cs
+++ MyWinForms/CategoryForm.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.tvCategory = new System.Windows.Forms.TreeView();
             this.btnAddCategory = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrioritet = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
             this.SuspendLayout();
             // 
             // tvCategory
@@ -42,20 +49,81 @@
             // 
             // btnAddCategory
             // 
-            this.btnAddCategory.Location = new System.Drawing.Point(1060, 28);
+            this.btnAddCategory.Location = new System.Drawing.Point(1044, 36);
             this.btnAddCategory.Margin = new System.Windows.Forms.Padding(4);
             this.btnAddCategory.Name = "btnAddCategory";
-            this.btnAddCategory.Size = new System.Drawing.Size(129, 41);
+            this.btnAddCategory.Size = new System.Drawing.Size(135, 50);
             this.btnAddCategory.TabIndex = 1;
             this.btnAddCategory.Text = "Додати";
             this.btnAddCategory.UseVisualStyleBackColor = true;
             this.btnAddCategory.Click += new System.EventHandler(this.btnAddCategory_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(513, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Назва";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(513, 44);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(433, 34);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(513, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пріорітет";
+            // 
+            // txtPrioritet
+            // 
+            this.txtPrioritet.Location = new System.Drawing.Point(513, 118);
+            this.txtPrioritet.Name = "txtPrioritet";
+            this.txtPrioritet.Size = new System.Drawing.Size(168, 34);
+            this.txtPrioritet.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(513, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Фото";
+            // 
+            // pbImage
+            // 
+            this.pbImage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbImage.Location = new System.Drawing.Point(513, 221);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(168, 155);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 5;
+            this.pbImage.TabStop = false;
+            this.pbImage.Click += new System.EventHandler(this.pbImage_Click);
             // 
             // CategoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1202, 517);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrioritet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAddCategory);
             this.Controls.Add(this.tvCategory);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -64,7 +132,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "CategoryForm";
             this.Load += new System.EventHandler(this.CategoryForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +142,11 @@
 
         private TreeView tvCategory;
         private Button btnAddCategory;
+        private Label label1;
+        private TextBox txtTitle;
+        private Label label2;
+        private TextBox txtPrioritet;
+        private Label label3;
+        private PictureBox pbImage;
     }
 }
--- MyWinForms/CategoryForm.cs
+++ MyWinForms/CategoryForm.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,27 +20,74 @@ namespace MyWinForms
             InitializeComponent();
         }
 
-        private void btnAddCategory_Click(object sender, EventArgs e)
+        private void LoadList()
         {
-            
-            Category category = new Category();
-            category.DateCreated = DateTime.Now;
-            category.Title = "ПК";
-            category.Priority = 1;
-            myContext.Categories.Add(category);
-            myContext.SaveChanges();
-
-        }
-
-        private void CategoryForm_Load(object sender, EventArgs e)
-        {
-            foreach(var c in myContext.Categories
-                .Where(x=>x.ParentId==null).ToList())
+            tvCategory.Nodes.Clear();
+            foreach (var c in myContext.Categories
+                .Where(x => x.ParentId == null).ToList())
             {
                 TreeNode node = new TreeNode(c.Title);
                 node.Tag = c;
                 node.Nodes.Add("");
                 tvCategory.Nodes.Add(node);
+            }
+        }
+        private void ClearInput()
+        {
+            txtPrioritet.Text = "0";
+            txtTitle.Text = "";
+            pbImage.Image = Image.FromFile($"images/upload.png");
+        }
+
+        private void btnAddCategory_Click(object sender, EventArgs e)
+        {
+            string dir = "images";
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+            Bitmap bitmap = new Bitmap(image);
+            string imageName = Path.GetRandomFileName() + ".jpg";
+            bitmap.Save(Path.Combine(dir, imageName), ImageFormat.Jpeg);
+
+            Category category = new Category();
+            category.DateCreated = DateTime.Now;
+            category.Title = txtTitle.Text;
+            category.Image = imageName;
+            category.Priority = int.Parse(txtPrioritet.Text);
+            myContext.Categories.Add(category);
+            myContext.SaveChanges();
+
+            ClearInput();
+            LoadList();
+
+        }
+
+        private void CategoryForm_Load(object sender, EventArgs e)
+        {
+            ClearInput();
+
+            LoadList();
+        }
+
+
+        
+        private string image;
+
+        private void pbImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.tif;...";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pbImage.Image = Image.FromFile(dlg.FileName);
+                    image = dlg.FileName;
+                }
+                catch
+                {
+                    MessageBox.Show("Файл не підтримується!");
+                }
+                
             }
         }
     }
