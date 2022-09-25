d54b4226f89b2a1d968c7bf8ebeabd706fbff0dc
novakvova <novakvova@gmail.com>
2022-09-25 15:41:20 +0300

    Add images to lvImages

--- WinFormsStepByStep/AddProductForm.Designer.cs
+++ WinFormsStepByStep/AddProductForm.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.lvImages = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAddImage = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -42,14 +46,45 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Додати товар";
             // 
+            // lvImages
+            // 
+            this.lvImages.Location = new System.Drawing.Point(15, 22);
+            this.lvImages.Name = "lvImages";
+            this.lvImages.Size = new System.Drawing.Size(623, 174);
+            this.lvImages.TabIndex = 1;
+            this.lvImages.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddImage);
+            this.groupBox1.Controls.Add(this.lvImages);
+            this.groupBox1.Location = new System.Drawing.Point(12, 84);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(745, 211);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Фото товара";
+            // 
+            // btnAddImage
+            // 
+            this.btnAddImage.Location = new System.Drawing.Point(651, 22);
+            this.btnAddImage.Name = "btnAddImage";
+            this.btnAddImage.Size = new System.Drawing.Size(82, 32);
+            this.btnAddImage.TabIndex = 2;
+            this.btnAddImage.Text = "Додати";
+            this.btnAddImage.UseVisualStyleBackColor = true;
+            this.btnAddImage.Click += new System.EventHandler(this.btnAddImage_Click);
+            // 
             // AddProductForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(769, 436);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Name = "AddProductForm";
             this.Text = "AddProductForm";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +93,8 @@
         #endregion
 
         private Label label1;
+        private ListView lvImages;
+        private GroupBox groupBox1;
+        private Button btnAddImage;
     }
 }
--- WinFormsStepByStep/AddProductForm.cs
+++ WinFormsStepByStep/AddProductForm.cs
@@ -15,6 +15,27 @@ namespace WinFormsStepByStep
         public AddProductForm()
         {
             InitializeComponent();
+            lvImages.LargeImageList = new ImageList();
+            lvImages.LargeImageList.ImageSize = new Size(64,64);
+
+        }
+
+        private void btnAddImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.tif;...";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string key = Guid.NewGuid().ToString();
+                ListViewItem item = new ListViewItem();
+                item.Tag = dlg.FileName;
+                item.Text = Path.GetFileName(dlg.FileName);
+                item.ImageKey = key;
+                lvImages.LargeImageList.Images.Add(key, Image.FromFile(dlg.FileName));
+                lvImages.Items.Add(item);
+
+                //MessageBox.Show("Select image " + dlg.FileName);
+            }
         }
     }
 }
