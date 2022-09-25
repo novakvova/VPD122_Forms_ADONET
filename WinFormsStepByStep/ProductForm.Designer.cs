d775c9e17c09b1016552388ee195d3b10f64eee1
novakvova <novakvova@gmail.com>
2022-09-25 14:50:36 +0300

    Drag and Drop begin

--- WinFormsStepByStep/ProductForm.Designer.cs
+++ WinFormsStepByStep/ProductForm.Designer.cs
@@ -30,34 +30,58 @@
         {
             this.lvProducts = new System.Windows.Forms.ListView();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnProductInfo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lvProducts
             // 
-            this.lvProducts.Location = new System.Drawing.Point(12, 82);
+            this.lvProducts.Location = new System.Drawing.Point(10, 62);
+            this.lvProducts.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lvProducts.MultiSelect = false;
             this.lvProducts.Name = "lvProducts";
-            this.lvProducts.Size = new System.Drawing.Size(920, 449);
+            this.lvProducts.Size = new System.Drawing.Size(806, 338);
             this.lvProducts.TabIndex = 0;
             this.lvProducts.UseCompatibleStateImageBehavior = false;
+            this.lvProducts.ListViewItemSorter = new ListViewIndexComparer();
+            this.lvProducts.InsertionMark.Color = Color.Green;
+            this.lvProducts.AllowDrop = true;
+            this.lvProducts.ItemDrag += new ItemDragEventHandler(lvProducts_ItemDrag);
+            this.lvProducts.DragEnter += new DragEventHandler(lvProducts_DragEnter);
+            this.lvProducts.DragOver += new DragEventHandler(lvProducts_DragOver);
+            this.lvProducts.DragLeave += new EventHandler(lvProducts_DragLeave);
+            this.lvProducts.DragDrop += new DragEventHandler(lvProducts_DragDrop);
+            this.lvProducts.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvProducts_MouseDoubleClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Location = new System.Drawing.Point(10, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 31);
+            this.label1.Size = new System.Drawing.Size(164, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Список продуктів";
             // 
+            // btnProductInfo
+            // 
+            this.btnProductInfo.Location = new System.Drawing.Point(641, 12);
+            this.btnProductInfo.Name = "btnProductInfo";
+            this.btnProductInfo.Size = new System.Drawing.Size(173, 44);
+            this.btnProductInfo.TabIndex = 2;
+            this.btnProductInfo.Text = "Інформація про товар";
+            this.btnProductInfo.UseVisualStyleBackColor = true;
+            this.btnProductInfo.Click += new System.EventHandler(this.btnProductInfo_Click);
+            // 
             // ProductForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 557);
+            this.ClientSize = new System.Drawing.Size(826, 418);
+            this.Controls.Add(this.btnProductInfo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lvProducts);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ProductForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -65,9 +89,15 @@
 
         }
 
+        private void LvProducts_DragEnter(object sender, DragEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         #endregion
 
         private ListView lvProducts;
         private Label label1;
+        private Button btnProductInfo;
     }
 }
--- WinFormsStepByStep/ProductForm.cs
+++ WinFormsStepByStep/ProductForm.cs
@@ -1,11 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.VisualBasic.Devices;
 using WinFormsStepByStep.Data;
+using WinFormsStepByStep.Data.Entities;
 
 namespace WinFormsStepByStep
 {
+    
     public partial class ProductForm : Form
     {
+        // Sorts ListViewItem objects by index.
+        private class ListViewIndexComparer : System.Collections.IComparer
+        {
+            public int Compare(object x, object y)
+            {
+                return ((ListViewItem)x).Index - ((ListViewItem)y).Index;
+            }
+        }
+
         private MyDataContext myData = new MyDataContext();
         public ProductForm()
         {
@@ -30,6 +42,7 @@ namespace WinFormsStepByStep
                     lvProducts.LargeImageList.Images.Add(id,
                         Image.FromFile($"images/{image}"));
                     ListViewItem item = new ListViewItem();
+                    item.Tag = p;
                     item.Text = $"{p.Name}\r\n{p.Price}";
                     item.ImageKey = $"{id}";
 
@@ -47,5 +60,119 @@ namespace WinFormsStepByStep
             
 
         }
+
+        private void btnProductInfo_Click(object sender, EventArgs e)
+        {
+            var listSelect = lvProducts.SelectedItems;
+            if(listSelect.Count > 0)
+            {
+                var item = listSelect[0];
+                var pImage = (Product)item.Tag;
+                MessageBox.Show("Product info: " + pImage.Id.ToString());
+            }
+            else
+            {
+                MessageBox.Show("ќбер≥ть товар");
+            }
+        }
+
+        private void lvProducts_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var listSelect = lvProducts.SelectedItems;
+            if (listSelect.Count > 0)
+            {
+                var item = listSelect[0];
+                var pImage = (Product)item.Tag;
+                MessageBox.Show("Product info: " + pImage.Id.ToString());
+            }
+        }
+
+
+        // Starts the drag-and-drop operation when an item is dragged.
+        private void lvProducts_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            lvProducts.DoDragDrop(e.Item, DragDropEffects.Move);
+        }
+
+        // Sets the target drop effect.
+        private void lvProducts_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.AllowedEffect;
+        }
+
+        // Moves the insertion mark as the item is dragged.
+        private void lvProducts_DragOver(object sender, DragEventArgs e)
+        {
+            // Retrieve the client coordinates of the mouse pointer.
+            Point targetPoint =
+                lvProducts.PointToClient(new Point(e.X, e.Y));
+
+            // Retrieve the index of the item closest to the mouse pointer.
+            int targetIndex = lvProducts.InsertionMark.NearestIndex(targetPoint);
+
+            // Confirm that the mouse pointer is not over the dragged item.
+            if (targetIndex > -1)
+            {
+                // Determine whether the mouse pointer is to the left or
+                // the right of the midpoint of the closest item and set
+                // the InsertionMark.AppearsAfterItem property accordingly.
+                Rectangle itemBounds = lvProducts.GetItemRect(targetIndex);
+                if (targetPoint.X > itemBounds.Left + (itemBounds.Width / 2))
+                {
+                    lvProducts.InsertionMark.AppearsAfterItem = true;
+                }
+                else
+                {
+                    lvProducts.InsertionMark.AppearsAfterItem = false;
+                }
+            }
+
+            // Set the location of the insertion mark. If the mouse is
+            // over the dragged item, the targetIndex value is -1 and
+            // the insertion mark disappears.
+            lvProducts.InsertionMark.Index = targetIndex;
+        }
+
+        // Removes the insertion mark when the mouse leaves the control.
+        private void lvProducts_DragLeave(object sender, EventArgs e)
+        {
+            lvProducts.InsertionMark.Index = -1;
+        }
+
+        // Moves the item to the location of the insertion mark.
+        private void lvProducts_DragDrop(object sender, DragEventArgs e)
+        {
+            // Retrieve the index of the insertion mark;
+            int targetIndex = lvProducts.InsertionMark.Index;
+
+            // If the insertion mark is not visible, exit the method.
+            if (targetIndex == -1)
+            {
+                return;
+            }
+
+            // If the insertion mark is to the right of the item with
+            // the corresponding index, increment the target index.
+            if (lvProducts.InsertionMark.AppearsAfterItem)
+            {
+                targetIndex++;
+            }
+
+            // Retrieve the dragged item.
+            ListViewItem draggedItem =
+                (ListViewItem)e.Data.GetData(typeof(ListViewItem));
+
+            // Insert a copy of the dragged item at the target index.
+            // A copy must be inserted before the original item is removed
+            // to preserve item index values. 
+            lvProducts.Items.Insert(
+                targetIndex, (ListViewItem)draggedItem.Clone());
+
+            // Remove the original copy of the dragged item.
+            lvProducts.Items.Remove(draggedItem);
+        }
+
+
+
     }
 }
